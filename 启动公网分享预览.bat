d2463802ffa621db1e2aaa2afa2cd1545b089f07
fx231023-afk <fx231023-afk@users.noreply.github.com>
2026-06-30 18:11:27 +0800

    Deploy GitHub Pages build

--- 启动公网分享预览.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\start-public-preview.ps1"
-pause
--- 启动本地预览.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-npm install
-npm run dev
-pause
